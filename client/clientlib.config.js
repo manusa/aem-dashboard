54d9ad89bf9d88c282687b6d9a925ebdcdccf010
Marc Nuri <marc@marcnuri.com>
2019-05-20 17:35:25 +0200

    #1: Created project structure
    - Project structure for server
    - Project structure for client

---
+++ client/clientlib.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  context: __dirname,
+  clientLibRoot: '../server/ui.apps/src/main/content/jcr_root/apps/marcnuri/dashboard/clientlibs',
+  libs: [
+    {
+      name: 'aem-dashboard',
+      allowProxy: true,
+      serializationFormat: 'xml',
+      categories: ['aem-dashboard'],
+      assets: {
+        js: [
+          'dist/bundle.js'
+        ],
+        css: []
+      }
+    }
+  ]
+};
